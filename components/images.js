328375af89c85cde631f215ac2700bf9a2689630
Paul_Killian <psk65lava@gmail.com>
2022-05-17 20:49:03 -0700

    restructuring code

---
+++ components/images.js
@@ -0,0 +1,51 @@
+import Charger  from '../public/Charger.png'
+import Camaro from '../public/Camaro.png'
+import Nomad from '../public/Nomad.png'
+import RoadRunner from '../public/RoadRunner.png'
+import Nova from '../public/Nova.png'
+import BelAir from '../public/BelAir.png'
+import Barracuda from '../public/Barracuda.png'
+
+export const images = [
+    { model: 
+      { 
+        name: 'Charger', 
+        modelImage: Charger, 
+        body: 'B-Body'} 
+      },
+    { 
+      model: 
+      { 
+        name: 'Camaro', 
+        modelImage: Camaro, 
+        body: 'B-Body' } 
+      },
+    { 
+      model: 
+      { 
+        name: 'Barracuda', 
+        modelImage: Barracuda, 
+        body: 'B-Body' } 
+      },
+    { 
+      model: 
+      { 
+        name: 'Nomad', 
+        modelImage: Nomad, 
+        body: 'B-Body' } 
+      },
+    { 
+      model: 
+      { 
+        name: 'Road Runner', 
+        modelImage: RoadRunner, 
+        body: 'B-Body' } 
+      },
+    { 
+      model: 
+      { 
+        name: 'Nova', 
+        modelImage: Nova, 
+        body: 'B-Body' } 
+      }
+  ]
