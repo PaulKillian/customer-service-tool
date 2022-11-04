8667c8a990b56e588a5010bed1a8c1414aea2eff
Paul_Killian <psk65lava@gmail.com>
2022-11-04 14:37:15 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+const withImages = require('next-images');
 
-module.exports = nextConfig
+module.exports = withCSS( withSass( withImages({
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+})));
