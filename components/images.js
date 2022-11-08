6ee474f15a9235457d2501585993670cb5647cae
Paul_Killian <psk65lava@gmail.com>
2022-11-08 15:38:38 -0800

    Update images.js

--- components/images.js
+++ components/images.js
@@ -1,63 +1,63 @@
-import Charger  from '../public/Charger.png'
-import Camaro from '../public/Camaro.png'
-import Nomad from '../public/Nomad.png'
-import RoadRunner from '../public/RoadRunner.png'
-import Nova from '../public/Nova.png'
-import BelAir from '../public/BelAir.png'
-import Barracuda from '../public/Barracuda.png'
-import Dart from '../public/Dart.png'
-import Impala from '../public/Impala.jpeg'
+// import Charger  from '../public/Charger.png'
+// import Camaro from '../public/Camaro.png'
+// import Nomad from '../public/Nomad.png'
+// import RoadRunner from '../public/RoadRunner.png'
+// import Nova from '../public/Nova.png'
+// import BelAir from '../public/BelAir.png'
+// import Barracuda from '../public/Barracuda.png'
+// import Dart from '../public/Dart.png'
+// import Impala from '../public/Impala.jpeg'
 
-export const images = [
-    { model: 
-      { 
-        name: 'Charger', 
-        modelImage: Charger, 
-        body: 'B-Body'} 
-      },
-    { 
-      model: 
-      { 
-        name: 'Camaro', 
-        modelImage: Camaro, 
-        body: 'F-Body' } 
-      },
-    { 
-      model: 
-      { 
-        name: 'Barracuda', 
-        modelImage: Barracuda, 
-        body: 'A-Body' } 
-      },
-    { 
-      model: 
-      { 
-        name: 'Nomad', 
-        modelImage: Nomad, 
-      },
-    { 
-      model: 
-      { 
-        name: 'Road Runner', 
-        modelImage: RoadRunner, 
-        body: 'B-Body' } 
-      },
-    { 
-      model: 
-      { 
-        name: 'Nova', 
-        modelImage: Nova, 
-      },
-    { 
-      model: 
-        { 
-            name: 'Dart', 
-            modelImage: Dart, 
-            body: 'B-Body'} 
-        },
-    { 
-      model: 
-        { 
-            name: 'Impala', 
-        }
-  ]
+// export const images = [
+//     { model: 
+//       { 
+//         name: 'Charger', 
+//         modelImage: Charger, 
+//         body: 'B-Body'} 
+//       },
+//     { 
+//       model: 
+//       { 
+//         name: 'Camaro', 
+//         modelImage: Camaro, 
+//         body: 'F-Body' } 
+//       },
+//     { 
+//       model: 
+//       { 
+//         name: 'Barracuda', 
+//         modelImage: Barracuda, 
+//         body: 'A-Body' } 
+//       },
+//     { 
+//       model: 
+//       { 
+//         name: 'Nomad', 
+//         modelImage: Nomad, 
+//       },
+//     { 
+//       model: 
+//       { 
+//         name: 'Road Runner', 
+//         modelImage: RoadRunner, 
+//         body: 'B-Body' } 
+//       },
+//     { 
+//       model: 
+//       { 
+//         name: 'Nova', 
+//         modelImage: Nova, 
+//       },
+//     { 
+//       model: 
+//         { 
+//             name: 'Dart', 
+//             modelImage: Dart, 
+//             body: 'B-Body'} 
+//         },
+//     { 
+//       model: 
+//         { 
+//             name: 'Impala', 
+//         }
+//   ]
