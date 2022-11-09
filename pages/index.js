e17ff738c4af40d93f5a1c87c9b932f75917abcc
Paul_Killian <psk65lava@gmail.com>
2022-11-09 13:24:52 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -109,7 +109,7 @@ export default function Home() {
           rows="4" 
           cols="50">
         </textarea>
-        <h2 style={{color: '#38A15C"}}>Click Year Of Vehicle</h2>
+        <h2 style={{color: "#38A15C"}}>Click Year Of Vehicle</h2>
         <RenderYear showYear={showYear} />
         <Button 
           currentYear={currentYear}
