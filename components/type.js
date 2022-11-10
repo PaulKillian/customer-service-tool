be12b99b5fd29ce13bd7cc6cec12ade463f6f7b8
Paul_Killian <psk65lava@gmail.com>
2022-11-10 15:05:45 -0800

    Update type.js

--- components/type.js
+++ components/type.js
@@ -1,15 +1,27 @@
-// import styles from '../styles/Home.module.css'
-// import { useState } from 'react'
+import styles from '../styles/Home.module.css'
+import { useState } from 'react'
 
-// export const type = () => {
-//     const [typeCar, setTypeCar] = useState([
-//         {
-//             1: ''
-//         }
-//     ])
-//     return (
-//         <select>
-
-//         </select>
-//     )
-// }
+export const bodyType = () => {
+  return (
+    <div>A Body</div>
+    <ul>
+      <li>Barracuda</li>
+      <li>Dart</li>
+      <li>Demon</li>
+      <li>Duster</li>
+      <li>Lancer</li>
+      <li>Scamp</li>
+      <li>Valiant</li>
+    </ul>
+    <div>B Body</div>
+    <ul>
+      <li>330 & 440</li>
+      <li>Dart</li>
+      <li>Demon</li>
+      <li>Duster</li>
+      <li>Lancer</li>
+      <li>Scamp</li>
+      <li>Valiant</li>
+    </ul>
+  )
+}
