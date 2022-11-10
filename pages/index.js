2a64d82d4e53d00b9db0bcdcd03f009da1626fde
Paul_Killian <psk65lava@gmail.com>
2022-11-10 13:41:37 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import Head from 'next/head'
 import Image from 'next/image'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../styles/Home.module.css'
 import { useState, useEffect, useRef, useLayoutEffect } from 'react'
 import 'animate.css';
