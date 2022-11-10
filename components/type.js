0e0ea4b88092047b450822c090d307b78ea18e82
Paul_Killian <psk65lava@gmail.com>
2022-11-10 15:23:22 -0800

    Update type.js

--- components/type.js
+++ components/type.js
@@ -1,27 +1,63 @@
-import styles from '../styles/Home.module.css'
-import { useState } from 'react'
+import styles from '../styles/BodyType.module.css'
+import { useState, useEffect } from 'react'
+import { gsap } from "gsap";
 
-export const bodyType = () => {
-  return (
-    <div>A Body</div>
-    <ul>
-      <li>Barracuda</li>
-      <li>Dart</li>
-      <li>Demon</li>
-      <li>Duster</li>
-      <li>Lancer</li>
-      <li>Scamp</li>
-      <li>Valiant</li>
-    </ul>
-    <div>B Body</div>
-    <ul>
-      <li>330 & 440</li>
-      <li>Dart</li>
-      <li>Demon</li>
-      <li>Duster</li>
-      <li>Lancer</li>
-      <li>Scamp</li>
-      <li>Valiant</li>
-    </ul>
+export const BodyType = (props) => {
+   useEffect(() => {
+    let ctx = gsap.context(() => {
+   
+    gsap.from(".box", {
+      opacity: 0, 
+      y: 100, 
+      duration: 1
+    })}, tArea);
+    
+    return () => ctx.revert(); // cleanup
+   }, []);
+  
+  {props.bodyTpe && 
+    return (
+    <div className={style.bodyFlex}>
+      <div>
+        <div>A Body</div>
+        <ul>
+          <li>Barracuda</li>
+          <li>Dart</li>
+          <li>Demon</li>
+          <li>Duster</li>
+          <li>Lancer</li>
+          <li>Scamp</li>
+          <li>Valiant</li>
+        </ul>
+      </div>
+      <div>
+        <div>B Body</div>
+        <ul>
+          <li>330 & 440</li>
+          <li>Belvedere</li>
+          <li>Charger</li>
+          <li>Coronet</li>
+          <li>Dart</li>
+          <li>Dayton</li>
+          <li>Fury</li>
+          <li>GTX</li>
+          <li>Polara</li>
+          <li>Road Runner</li>
+          <li>Satellite</li>
+          <li>Savoy</li>
+          <li>Sport Fury</li>
+          <li>Super Bee</li>
+          <li>Super Bird</li>
+        </ul>
+      </div>
+      <div>
+        <div>E Body</div>
+        <ul>
+          <li>Challenger</li>
+          <li>Cuda</li>
+        </ul>
+      </div>
+    </div>
   )
+ }
 }
