e1ea57f66b0109e7ea5ceac05e3ee7d071cbb992
Paul_Killian <psk65lava@gmail.com>
2022-11-10 08:21:57 -0800

    Update data.js

--- components/data.js
+++ components/data.js
@@ -4,5 +4,9 @@ export const Impala = [
     'upper_RH_eyebrow_moldings': '827132',
     'upper_LH_eyebrow_moldings': '827131',
     'front_bumber': 'B17053',
+    'molding': {
+      'windshield': 'K121',
+      'back_glass': 'K123'
+    }
   }
 ]
