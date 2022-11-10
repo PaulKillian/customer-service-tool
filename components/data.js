d919dbc1aa337580d485f06f97be09c9abebee2f
Paul_Killian <psk65lava@gmail.com>
2022-11-10 13:00:02 -0800

    Update data.js

--- components/data.js
+++ components/data.js
@@ -1,21 +1,21 @@
-export const impala = [
-  {
-    'year': 63,
-    'upper_RH_eyebrow_moldings': '827132',
-    'upper_LH_eyebrow_moldings': '827131',
-    'front_bumber': 'B17053',
-    'molding': {
-      'windshield': 'K121',
-      'back_glass': 'K123'
-    },
-  }
-]
+// export const impala = [
+//   {
+//     'year': 63,
+//     'upper_RH_eyebrow_moldings': '827132',
+//     'upper_LH_eyebrow_moldings': '827131',
+//     'front_bumber': 'B17053',
+//     'molding': {
+//       'windshield': 'K121',
+//       'back_glass': 'K123'
+//     },
+//   }
+// ]
 
-export const nova = [
-  { 
-    'year': 68,
-    'year': 79,
-    'bow_tie_outer_mirror': 'CM3005',
-    'door_glove_trunk_lock_set: 'TK155',
-  }
-]
+// export const nova = [
+//   { 
+//     'year': 68,
+//     'year': 79,
+//     'bow_tie_outer_mirror': 'CM3005',
+//     'door_glove_trunk_lock_set: 'TK155',
+//   }
+// ]
