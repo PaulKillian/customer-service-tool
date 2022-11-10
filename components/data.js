dd7134a227c3cf15446d79aecf6675a197d20c30
Paul_Killian <psk65lava@gmail.com>
2022-11-10 12:29:18 -0800

    Update data.js

--- components/data.js
+++ components/data.js
@@ -1,13 +1,13 @@
 export const impala = [
   {
-    'year': 63
+    'year': 63,
     'upper_RH_eyebrow_moldings': '827132',
     'upper_LH_eyebrow_moldings': '827131',
     'front_bumber': 'B17053',
     'molding': {
       'windshield': 'K121',
       'back_glass': 'K123'
-    }
+    },
   }
 ]
 
@@ -17,6 +17,5 @@ export const nova = [
     'year': 79,
     'bow_tie_outer_mirror': 'CM3005',
     'door_glove_trunk_lock_set: 'TK155',
-    
   }
 ]
