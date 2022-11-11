9729526d345936eb8582317c13aa2aad2b7fa7ff
Paul_Killian <psk65lava@gmail.com>
2022-11-10 16:54:38 -0800

    Update bodytype.js

--- components/bodytype.js
+++ components/bodytype.js
@@ -17,44 +17,46 @@ const BodyType = () => {
   
     return (
     <div className={'bodyFlex'}>
-      <div>
-        <div>A Body</div>
-        <ul>
-          <li>Barracuda</li>
-          <li>Dart</li>
-          <li>Demon</li>
-          <li>Duster</li>
-          <li>Lancer</li>
-          <li>Scamp</li>
-          <li>Valiant</li>
-        </ul>
-      </div>
-      <div>
-        <div>B Body</div>
-        <ul>
-          <li>330 & 440</li>
-          <li>Belvedere</li>
-          <li>Charger</li>
-          <li>Coronet</li>
-          <li>Dart</li>
-          <li>Dayton</li>
-          <li>Fury</li>
-          <li>GTX</li>
-          <li>Polara</li>
-          <li>Road Runner</li>
-          <li>Satellite</li>
-          <li>Savoy</li>
-          <li>Sport Fury</li>
-          <li>Super Bee</li>
-          <li>Super Bird</li>
-        </ul>
-      </div>
-      <div>
-        <div>E Body</div>
-        <ul>
-          <li>Challenger</li>
-          <li>Cuda</li>
-        </ul>
+       <div className={style.bodyFlex}>
+         <div>
+           <div>A Body</div>
+           <ul>
+             <li>Barracuda</li>
+             <li>Dart</li>
+             <li>Demon</li>
+             <li>Duster</li>
+             <li>Lancer</li>
+             <li>Scamp</li>
+             <li>Valiant</li>
+           </ul>
+         </div>
+         <div>
+           <div>B Body</div>
+           <ul>
+             <li>330 & 440</li>
+             <li>Belvedere</li>
+             <li>Charger</li>
+             <li>Coronet</li>
+             <li>Dart</li>
+             <li>Dayton</li>
+             <li>Fury</li>
+             <li>GTX</li>
+             <li>Polara</li>
+             <li>Road Runner</li>
+             <li>Satellite</li>
+             <li>Savoy</li>
+             <li>Sport Fury</li>
+             <li>Super Bee</li>
+             <li>Super Bird</li>
+           </ul>
+         </div>
+         <div>
+           <div>E Body</div>
+           <ul>
+             <li>Challenger</li>
+             <li>Cuda</li>
+           </ul>
+         </div>
       </div>
     </div>
   )
