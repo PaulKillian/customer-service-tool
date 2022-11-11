6fdbd14569fd6ae68f91a719576ef2bfc2b3bee8
Paul_Killian <psk65lava@gmail.com>
2022-11-11 12:29:20 -0800

    Update opg.js

--- components/opg.js
+++ components/opg.js
@@ -3,7 +3,9 @@ import { useState, useEffect, useRef } from 'react'
 import { gsap } from "gsap";
 
 const OPG = () => {
-   const cars = 'Bonneville(59-76)\n Cadillac(54-76)\n Catalina(59-76)\n Chevelle(64-77)\n Cutlass(61-77)\n El Camino(64-87)\n Grand Prix(62-77)\n GTO(64-73\n Lemans(61-73)\n Malibu(78-83)\n Monte Carlo(71-88)\n Riviera(63-76) \n Skylark(61-72)\n Tempest(61-71)'
+   let cars = 'Bonneville(59-76)\n Cadillac(54-76)\n Catalina(59-76)\n Chevelle(64-77)\n Cutlass(61-77)\n El Camino(64-87)\n Grand Prix(62-77)\n GTO(64-73\n Lemans(61-73)\n Malibu(78-83)\n Monte Carlo(71-88)\n Riviera(63-76) \n Skylark(61-72)\n Tempest(61-71)'
+   cars.replace(/\n/g, "<br />")
+   
    useEffect(() => {
     const opg = document.querySelector('opg')
       
