ade360137d452cca53fd6f525d93b5c9921b061b
Paul_Killian <psk65lava@gmail.com>
2022-11-11 08:37:01 -0800

    Update opg.js

--- components/opg.js
+++ components/opg.js
@@ -21,7 +21,7 @@ const BodyType = () => {
          <div>
            <h4 className={styles.h4Design}>A</h4>
              <h3>Bonneville(59-76), Catalina(59-76), Chevelle(64-77), Cutlass(61-77), 
-                Duster, Lancer, Scamp</h3>
+                El Camino(64-87), Grand Prix(62-77), GTO(64-73)</h3>
          </div>
          <div>
            <h4 className={styles.h4Design}>B</h4>
