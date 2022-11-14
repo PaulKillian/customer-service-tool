6c992660cb6abec866b75d8e533334be369dae81
Paul_Killian <psk65lava@gmail.com>
2022-11-14 08:36:38 -0800

    Create returnLabel.js

---
+++ components/returnLabel.js
@@ -0,0 +1,33 @@
+import styles from '../styles/BodyType.module.css'
+import { useState, useEffect, useRef } from 'react'
+import { gsap } from "gsap";
+
+const ReturnLabel = () => {
+   useEffect(() => {
+    const returnDiv = document.querySelector('return')
+      
+    let ctx = gsap.context(() => {
+    gsap.from(".bodyFlex", {
+      opacity: 0, 
+      x: 100, 
+      duration: 1
+    })}, returnDiv);
+    return () => ctx.revert(); // cleanup
+   }, []);
+  
+    return (
+     <div id={'return'}>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc2IB5w3jt28UKSCdvMHe2PeZ-N-d9BkOCU5Qv_c5s6ynnjJA/viewform" 
+          width="640" 
+          height="360" 
+          frameborder="0" 
+          webkitallowfullscreen="" 
+          mozallowfullscreen="" 
+          allowfullscreen="">
+        </iframe>
+     </div>
+  )
+}
+
+export default ReturnLabel;
+
