f9cc505c7e466ae34c57632146b7eab3b41e4e4f
Paul_Killian <psk65lava@gmail.com>
2022-11-14 14:21:25 -0800

    Update buttons.js

--- components/buttons.js
+++ components/buttons.js
@@ -1,6 +1,6 @@
 import styles from '../styles/Home.module.css'
 import { useState } from 'react'
-import { InfoRender } from './info.js
+import { InfoRender } from './info.js'
 
 
 export const ButtonClear = () => {
