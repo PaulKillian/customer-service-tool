868cfdf2155daa8ad18fe4fa64151bfa34654f90
Paul_Killian <psk65lava@gmail.com>
2022-11-14 13:38:48 -0800

    Update infoRender.js

--- components/infoRender.js
+++ components/infoRender.js
@@ -3,7 +3,7 @@ import { useState, useEffect, useRef } from 'react'
 import { gsap } from "gsap";
 import { returnLabel, bodyType, opg, deliveryTimes } from './components/info.js'
 
-const infoRender = () => {
+const infoRender = (returnedInfo) => {
    useEffect(() => {
     const infoDiv = document.querySelector('info')
       
