00a59caa4cb7d001b2276ac7d1daa427994d180b
Paul_Killian <psk65lava@gmail.com>
2022-11-14 14:25:10 -0800

    Update extensions.js

--- components/extensions.js
+++ components/extensions.js
@@ -1,17 +1,17 @@
 const owner = [
    { 
       department: 'Owner',
-      name: Jeff Leonard*, ext: 1201,
-      name: Jeff Leonard*, ext: 1202 
+      name: 'Jeff Leonard*', ext: 1201,
+      name: 'Jeff Leonard*', ext: 1202 
    },
    {
       department: 'Administrative Asst',
-      name: Michaelle Shi*, ext: 1221,
-      name: Michaelle Shi Remote*, ext: 1225
+      name: 'Michaelle Shi*', ext: 1221,
+      name:' Michaelle Shi Remote*', ext: 1225
    },
    {
     department: 'General Manager',
-    name: Mark Vogt*, ext: 1221,
-    name: Conference room, ext: 1225
+    name: 'Mark Vogt*', ext: 1221,
+    name: 'Conference room', ext: 1225
    }
  ]
