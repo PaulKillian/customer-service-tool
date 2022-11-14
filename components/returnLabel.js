4ce60297ddd83af28e7c620141311f4718bea1e7
Paul_Killian <psk65lava@gmail.com>
2022-11-14 10:59:14 -0800

    Update returnLabel.js

--- components/returnLabel.js
+++ components/returnLabel.js
@@ -16,7 +16,7 @@ const ReturnLabel = () => {
    }, []);
   
     return (
-     <div id={'return'}>
+     <div className={'return'}>
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc2IB5w3jt28UKSCdvMHe2PeZ-N-d9BkOCU5Qv_c5s6ynnjJA/viewform" 
           width="640" 
           height="700" 
