5adb8b23e6ba59ad0bf3238902e13bf2103a3c99
Paul_Killian <psk65lava@gmail.com>
2022-11-14 13:26:49 -0800

    Create info.js

---
+++ components/info.js
@@ -0,0 +1,52 @@
+export const bodyType = () => {
+  return (
+    <div className={'info'} >
+      <div className={styles.bodyFlex}>
+        <div>
+          <h3 className={styles.bodyMargin}>
+            <span className={styles.h4Design}>A</span><br/> 
+            Barracuda<br/> Dart<br/> Demon<br/> Duster<br/> Lancer<br/> Scamp
+          </h3>
+        </div>
+        <div>
+          <h3 className={styles.bodyMargin}>
+          <span className={styles.h4Design}>B</span><br/> 
+            330 & 440<br/> 
+            Belvedere<br/> 
+            Charger<br/> 
+            Coronet<br/> 
+            Dayton<br/> 
+            Fury<br/>
+            GTX<br/> 
+            Polara<br/> 
+            Road Runner<br/> 
+            Savoy<br/> 
+            Sport Fury<br/> 
+            Super Bee<br/> 
+            Super Bird
+          </h3>
+        </div>
+        <div>
+          <h3 className={styles.bodyMargin}>
+            <span className={styles.h4Design}>E</span><br/> 
+            Challenger<br/> Cuda
+          </h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const deliveryTimes = () => {
+  return (
+    <div className={'info'}>
+       <h3>
+        <span className={styles.h4Design}>Zone</span><br/>
+        2 & 3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2-3&nbsp;days<br/> 
+        4 & 5&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3-5&nbsp;days<br/>
+        6&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;5-7&nbsp;days<br/> 
+        7 & 8&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;7-10&nbsp;days
+       </h3>
+    </div>
+  )
+}
