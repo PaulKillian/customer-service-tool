c76609db34b2a736eca287eff0ac9c5b9fc428b7
Paul_Killian <psk65lava@gmail.com>
2022-11-14 11:56:32 -0800

    Create extensions.js

---
+++ components/extensions.js
@@ -0,0 +1,15 @@
+const owner = [
+  {
+    name: Jeff Leonard*,
+    name: Jeff Leonard*,
+    ext: 1201,
+    ext: 1202,
+  },
+  {
+    name: Jeff Leonard*,
+    name: Jeff Leonard*,
+    ext: 1201,
+    ext: 1202,
+  },
+              
+              ]
