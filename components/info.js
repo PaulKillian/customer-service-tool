1cfd58f4dc50ff68df7e3c60ece98223facea6ce
Paul_Killian <psk65lava@gmail.com>
2022-11-14 13:28:57 -0800

    Update info.js

--- components/info.js
+++ components/info.js
@@ -50,3 +50,20 @@ export const deliveryTimes = () => {
     </div>
   )
 }
+
+export const opg = () => {
+   return (
+    <div className={'info'}>
+        <h3><span className={styles.h4Design}>CAR</span><br/> 
+            Bonneville(59-76)<br/> Cadillac(54-76)<br/>
+            Catalina(59-76)<br/> Chevelle(64-77)<br/> 
+            Cutlass(61-77)<br/> El Camino(64-87)<br/> 
+            Grand Prix(62-77)<br/> GTO(64-73)<br/> 
+            Lemans(61-73)<br/> Malibu(78-83)<br/> 
+            Monte Carlo(71-88)<br/> Riviera(63-76) 
+            <br/> Skylark(61-72)<br/> Tempest(61-71)
+            <br/><br/>Phone# 1‑800‑243‑8355
+        </h3>
+    </div>
+  )
+}
