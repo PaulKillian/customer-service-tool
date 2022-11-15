626842553389459fda4c5a29747b1a2c12742b33
Paul_Killian <psk65lava@gmail.com>
2022-11-15 09:10:30 -0800

    years change

--- components/extensions.js
+++ components/extensions.js
@@ -10,17 +10,17 @@ const owner = [
       name:' Michaelle Shi Remote*', ext: 1225
    },
    {
-    department: 'General Manager',
-    name: 'Mark Vogt*', ext: 1215,
-    name: 'Conference room', ext: 1231,
-    name: 'Hub room', ext: 1232
+      department: 'General Manager',
+      name: 'Mark Vogt*', ext: 1215,
+      name: 'Conference room', ext: 1231,
+      name: 'Hub room', ext: 1232
    },
    {
-    department: 'Hoover Building #2',
-    name: 'Office Station', ext: 1250,
-    name: 'Shipping Station', ext: 1251,
-    name: 'Tech Shop', ext: 1254,
-    name: 'Tech Desk', ext: 1255
+      department: 'Hoover Building #2',
+      name: 'Office Station', ext: 1250,
+      name: 'Shipping Station', ext: 1251,
+      name: 'Tech Shop', ext: 1254,
+      name: 'Tech Desk', ext: 1255
    },
    {
       department: 'Monarch Building',
--- components/year.js
+++ components/year.js
@@ -2,22 +2,18 @@ import styles from '../styles/Home.module.css'
 import { useState } from 'react'
 
 export const RenderYear = (props) => {
-    const [vehicleYears, setVehicleYears] = useState([]);
-
-    const yearComponent = () => {
-       const years = [];
-       for(let i = 0; i < 106; i++) {
-           years.push(i)
-       }
-       setVehicleYears(years)
-    };
-    
-    yearComponent()
+    const years = [
+        47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+        61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
+        75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88,
+        89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, '00', '01', 
+        '02', '03', '04', '05', '06', '07',
+      ];
 
     return (
         <div className={styles.yearWidth}>
           <ul className={styles.flex}>
-            {vehicleYears.map((year) => (
+            {years.map((year) => (
               <li 
                 key={year}
                 onClick={e => props.showYear(e.target.innerText)}
