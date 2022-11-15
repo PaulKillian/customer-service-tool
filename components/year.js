4981a34b2bddf66298701020ceacd172bab2d23b
Paul_Killian <psk65lava@gmail.com>
2022-11-15 08:40:48 -0800

    Update year.js

--- components/year.js
+++ components/year.js
@@ -2,14 +2,14 @@ import styles from '../styles/Home.module.css'
 import { useState } from 'react'
 
 export const RenderYear = (props) => {
-    const [vehicleYears, setVehicleYears] = useState(0);
+    const [vehicleYears, setVehicleYears] = useState([]);
 
     const yearComponent = () => {
        const years = [];
        for(i = 0; i < 106; i++) {
            years.push(i)
        }
-       setVehicleYears([years])
+       setVehicleYears(years)
     };
 
     return (
