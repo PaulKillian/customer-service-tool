a7bde6a7dd242f9b3e14690f4602aaa46bd2385d
Paul_Killian <psk65lava@gmail.com>
2022-11-15 08:01:02 -0800

    Update extensions.js

--- components/extensions.js
+++ components/extensions.js
@@ -11,7 +11,15 @@ const owner = [
    },
    {
     department: 'General Manager',
-    name: 'Mark Vogt*', ext: 1221,
-    name: 'Conference room', ext: 1225
+    name: 'Mark Vogt*', ext: 1215,
+    name: 'Conference room', ext: 1231,
+    name: 'Hub room', ext: 1232
+   },
+   {
+    department: 'Hoover Building #2',
+    name: 'Office Station', ext: 1250,
+    name: 'Shipping Station', ext: 1251,
+    name: 'Tech Shop', ext: 1254,
+    name: 'Tech Desk', ext: 1255
    }
  ]
