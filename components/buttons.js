4242c520ad276f5f7319e71ff91abe0aa8275ada
Paul_Killian <psk65lava@gmail.com>
2022-11-21 15:26:41 -0800

    Update buttons.js

--- components/buttons.js
+++ components/buttons.js
@@ -61,7 +61,7 @@ export const ButtonGetClearedData = (props) => {
   return (
     <button 
       className={styles.button28}
-      onClick=dangerouslySetInnerHTML={{__html: getData()}}
+      onClick={getData}
     >get cleared data
     </button>
   )
