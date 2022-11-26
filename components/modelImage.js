cb5d52c6cd5e5a6da3c80fb213103a41deaf523f
Paul_Killian <psk65lava@gmail.com>
2022-11-26 11:02:10 -0800

    Update modelImage.js

--- components/modelImage.js
+++ components/modelImage.js
@@ -10,7 +10,7 @@ export const ModelImage = (props) => {
             height={400}
             objectFit='cover'
             alt={'car'}
-            src={`/${props.currentModel}`}
+            src={`/${props.currentModel}.webp`}
           />
         </div>
       }
