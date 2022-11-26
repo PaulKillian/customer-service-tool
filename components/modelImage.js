2d7ea9285510194355e674e0a12d8a7e397de1a9
Paul_Killian <psk65lava@gmail.com>
2022-11-25 16:55:06 -0800

    Update modelImage.js

--- components/modelImage.js
+++ components/modelImage.js
@@ -3,14 +3,15 @@ import styles from '../styles/Home.module.css'
 
 export const ModelImage = (props) => {
     return (
-      <div className={styles.imgDim}>
-        <Image 
-          width={655}
-          height={400}
-          objectFit='cover'
-          alt={'car'}
-          src={`/${props.currentModel}`}
-        />
+        {props.currentModel && props.currentYear}
+        <div className={styles.imgDim}>
+          <Image 
+            width={655}
+            height={400}
+            objectFit='cover'
+            alt={'car'}
+            src={`/${props.currentModel}`}
+         />
       </div>
     )
   }
