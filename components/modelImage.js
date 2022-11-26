e178c79d3447a64252288e76d00198b6447ce2ce
Paul_Killian <psk65lava@gmail.com>
2022-11-26 11:34:28 -0800

    Update modelImage.js

--- components/modelImage.js
+++ components/modelImage.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export const ModelImage = (props) => {
+    console.log({`/${props.currentYear}, ${props.currentModel}.webp`}
     return (
         <div>
         {props.currentModel && props.currentYear
