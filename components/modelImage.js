4a2482157959c68df5b7305c6bf9c6cc4e75b266
Paul_Killian <psk65lava@gmail.com>
2022-11-26 11:26:50 -0800

    Update modelImage.js

--- components/modelImage.js
+++ components/modelImage.js
@@ -2,17 +2,20 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export const ModelImage = (props) => {
-    console.log
     return (
-      if (props.currentModel && props.currentYear) {
-        <div className={styles.imgDim}>
-          <Image 
-            width={655}
-            height={400}
-            objectFit='cover'
-            alt={'car'}
-            src={`/${props.currentModel}.webp`}
-          />
+        <div>
+        {props.currentModel && props.currentYear
+            ? <div className={styles.imgDim}>
+                  <Image 
+                    width={655}
+                    height={400}
+                    objectFit='cover'
+                    alt={'car'}
+                    src={`/${props.currentModel}.webp`}
+                  />
+              </div>
+            : null
+         {
         </div>
       }
     )
