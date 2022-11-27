9f89d82d88491775b5f32918e9a0ee4c8267f2d6
Paul_Killian <psk65lava@gmail.com>
2022-11-26 22:01:47 -0800

    more images

--- components/modelImage.js
+++ components/modelImage.js
@@ -1,35 +1,35 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { gsap } from "gsap";
 
 export const ModelImage = (props) => {
-    useEffect(() => {
-      const carImg = document.querySelector('carImg') 
-      let ctx = gsap.context(() => {
-        gsap.from(".carImg", {
-          opacity: 0, 
-          duration: 1
-        })}, carImg);
-      return () => ctx.revert();
-    })
-    
-    return (
-        <div>
-        {props.currentModel && props.currentYear
-            ? <div className={styles.imgDim}>
-                  <Image 
-                    className='carImg'
-                    width={655}
-                    height={400}
-                    objectFit='cover'
-                    alt={'car'}
-                    loading='eager'
-                    src={`/${props.currentYear} ${props.currentModel}.webp`}
-                  />
-              </div>
-            : null
-         }
-        </div>
-     )
+  useEffect(() => {
+    const carImg = document.querySelector('carImg') 
+    let ctx = gsap.context(() => {
+      gsap.from(".carImg", {
+        opacity: 0, 
+        duration: 1
+      })}, carImg);
+    return () => ctx.revert();
+  })
+  
+  return (
+      <div>
+      {props.currentModel && props.currentYear
+          ? <div className={styles.imgDim}>
+              <Image 
+                className='carImg'
+                width={655}
+                height={400}
+                objectFit='cover'
+                alt={'car'}
+                loading='eager'
+                src={`/${props.currentYear} ${props.currentModel}.webp`}
+              />
+            </div>
+          : null
+        }
+      </div>
+    )
   }
