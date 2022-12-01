54cb1c2e4c1f76d5751bb6332bbb672ea33f5a24
Paul_Killian <psk65lava@gmail.com>
2022-12-01 12:34:52 -0800

    Update modelImage.js

--- components/modelImage.js
+++ components/modelImage.js
@@ -42,7 +42,7 @@ export const ModelImage = (props) => {
              : <Image
                 id={'img'}
                 width={655}
-                height={300}
+                height={400}
                 objectFit='cover'
                 alt={'car'}
                 loading='eager'
