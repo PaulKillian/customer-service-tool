29ca0232319a87963bc516e1bb60116d895a61e5
Paul_Killian <psk65lava@gmail.com>
2022-12-07 11:29:01 -0800

    Update parts.js

--- components/parts.js
+++ components/parts.js
@@ -1,15 +1,21 @@
+import "./styles.css";
+import { useState } from "react";
+
 const Parts = () => {
-  const impala =  {
-    '64': { SS_Side_Molding: https://www.classicindustries.com/product/1964/chevrolet/impala/parts/b9206.html },
-    '62': { Drop_Spindles: 
-         {
-           link: https://www.classicindustries.com/product/1964/chevrolet/impala/parts/bs7001.html,
-           link: https://www.classicindustries.com/product/1964/chevrolet/impala/parts/153638.html
-         }
-      }
+  const [impala64, setImpala64] = useState({
+    SS_Side_Molding:
+      "https://www.classicindustries.com/product/1964/chevrolet/impala/parts/b9206.html"
+  });
+  const [impala62, setImpala62] = useState({
+    Drop_Spindles: {
+      link:
+        "https://www.classicindustries.com/product/1964/chevrolet/impala/parts/bs7001.html",
+      link:
+        "https://www.classicindustries.com/product/1964/chevrolet/impala/parts/153638.html"
     }
+  });
+
+  return <a href={impala64.SS_Side_Molding}>SS Side Moldings</a>;
 };
 
 export default Parts;
-
-  
