8ee62bed7f3969c650618429eeceea4f06927608
Paul_Killian <psk65lava@gmail.com>
2022-12-07 14:49:54 -0800

    Update parts.js

--- components/parts.js
+++ components/parts.js
@@ -1,18 +1,16 @@
-import { useState } from "react";
-
 const parts = () => {
-  const [impala64, setImpala64] = useState({
+  const impala64 = {
     SS_Side_Molding:
       "https://www.classicindustries.com/product/1964/chevrolet/impala/parts/b9206.html"
-  });
-  const [impala62, setImpala62] = useState({
+  };
+  const impala62 = {
     Drop_Spindles: {
       link:
         "https://www.classicindustries.com/product/1964/chevrolet/impala/parts/bs7001.html",
       link:
         "https://www.classicindustries.com/product/1964/chevrolet/impala/parts/153638.html"
     }
-  });
+  };
 
   return <a href={impala64.SS_Side_Molding}>SS Side Moldings</a>;
 };
