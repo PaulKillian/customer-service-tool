8b2fcf819f7c3ffb608a0498d176b823ddc5143c
Paul_Killian <psk65lava@gmail.com>
2022-12-07 08:02:19 -0800

    Update parts.js

--- components/parts.js
+++ components/parts.js
@@ -3,7 +3,6 @@ export const Parts = () = > {
     '64': {
           SSsideMolding: {
           link: https://www.classicindustries.com/product/1964/chevrolet/impala/parts/b9206.html
-          img: '../public/ssSideMolding.jpg
         }
       }
     }
