51a34dbfc965bbef1a1f814732e649f4191a00ef
Paul_Killian <psk65lava@gmail.com>
2022-12-07 08:35:03 -0800

    Update parts.js

--- components/parts.js
+++ components/parts.js
@@ -1,19 +1,13 @@
 export const Parts = () = > {
   const impala =  {
-    '64': 
-       {
-         SS_Side_Molding: {
-         link: https://www.classicindustries.com/product/1964/chevrolet/impala/parts/b9206.html
-       },
-    '62': 
-       {
-         Drop_Spindles: 
+    '64': SS_Side_Molding: https://www.classicindustries.com/product/1964/chevrolet/impala/parts/b9206.html,
+    '62': Drop_Spindles: 
          {
            link: https://www.classicindustries.com/product/1964/chevrolet/impala/parts/bs7001.html,
            link: https://www.classicindustries.com/product/1964/chevrolet/impala/parts/153638.html
          }
       }
-    }
+   }
 }
 
   
