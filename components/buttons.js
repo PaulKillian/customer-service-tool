e45585610c6715d5f5debfa547ccb7fb9afef488
Paul_Killian <psk65lava@gmail.com>
2022-12-07 16:31:34 -0800

    Update buttons.js

--- components/buttons.js
+++ components/buttons.js
@@ -97,39 +97,3 @@ export const ButtonGetClearedData = (props) => {
 //     </button>
 //   )
 // }
-
-export const ButtonParts = (props) => {
-  const yearAndModel = `${props.currentYear, props.currentModel}`
-  let partName = ''
-  let links = []
-  switch (yearAndModel) {
-    case '62 Impala':
-      partName = 'Drop Spindle'
-      links = [{
-          link: "https://www.classicindustries.com/product/1964/chevrolet/impala/parts/bs7001.html",
-          link: "https://www.classicindustries.com/product/1964/chevrolet/impala/parts/153638.html"
-        }]
-    break;
-    case '64 Impala': 
-      partName = 'SS Side Molding Set'
-      links = [{
-          link: "https://www.classicindustries.com/product/1964/chevrolet/impala/parts/b9206.html"
-        }]
-    break;
-    default:
-      null
-  }
-
-  return (
-    <div>
-      {links.map((link, index) => (
-        <a 
-          href={link} 
-          key={index}
-        >{partName}</a>
-      ))}
-    </div>
-  )
-};
-
-export default ButtonParts;
