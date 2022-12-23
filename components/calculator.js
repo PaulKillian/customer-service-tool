51a54fb0cbfb13990ca48f83499d5877c4ec1fc3
Paul_Killian <psk65lava@gmail.com>
2022-12-23 14:23:45 -0800

    calculator

--- components/calculator.js
+++ components/calculator.js
@@ -1,29 +1,13 @@
-import Calculator from "awesome-react-calculator";
-
-const style = {
-  height: '24rem',
-  width: '15rem'
-}
+import { Calculator } from "react-calculatxr";
+import "react-calculatxr/dist/react-calculatxr.css";
+import styles from '../styles/cal.module.css'
 
 const cal = () => {
-  const handleInput = input => {
-    console.log(`${input.expression} is shown in the calculator, User clicked the ${input.key}`)
-  }
-
-  const onResultChange = newResult => {
-    console.log(newResult)
-    console.log(`${newResult.expression} is validated as ${newResult.result} `)
-  }
- 
-  return (
-    <div style={style}>
-      <h1>Calculator</h1>
-      <Calculator
-        onNewInput={handleInput}
-        onResultChange={onResultChange}
-      />
-    </div>
-  )
+    return (
+      <div className={styles.cal}>
+        <Calculator /> 
+      </div> 
+    )
 }
 
 export default cal
